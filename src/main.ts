5ce80fb241c9256f0b02e0b8a1f82d8b51847685
d-Jhon-b <wwluiza.09@gmail.com>
2026-05-17 18:53:02 -0300

    fix(cors)
    Solve cors secure values

--- src/main.ts
+++ src/main.ts
@@ -3,18 +3,42 @@ import * as dotenv from 'dotenv';
 
 import { RainSafeBroker } from "./service/mqqtService";
 import { RegisterRoutes } from "./routes/routes";
-
+import cors from "cors";
 import swaggerUi from "swagger-ui-express";
 
 dotenv.config();
 
+
+const allowUrls = [
+
+
+]
 async function bootstrap() {
   const app = express();
+
+  const allowedOrigins = [
+    "http://localhost:5173",           
+    "http://localhost:3000",            
+    "https://rainsafe-web.vercel.app"   
+  ];
+
+  const corsOptions: cors.CorsOptions = {
+    origin: (origin, callback) => {
+      if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+        callback(null, true);
+      } else {
+        callback(new Error("Acesso negado pelo CORS: Origem não permitida."));
+      }
+    },
+    methods: ["GET", "POST", "PUT", "DELETE"], 
+    allowedHeaders: ["Content-Type", "Authorization"]
+  };
+  app.use(cors(corsOptions));
   app.use(express.json());
 
   const broker = new RainSafeBroker();
   broker.connect();
-
+  
   app.use("/docs", swaggerUi.serve, async (_req: any, res: any) => {
     try {
       const swaggerDocument = await import("./routes/swagger.json");
